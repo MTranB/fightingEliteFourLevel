20c84a0a6cd4efa9e5f845db45037281f253b82d
MTranB <burtontranmicah@gmail.com>
2024-12-09 01:35:37 -0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,13 +1,14 @@
-siteURL = "https://mtranb.github.io/fightingEliteFourLevel/";
 //checks the src of the image and changes it to the next image using parameterized function
 function chatBoxes(chatImage) {
-  console.log(chatImage);
-  if (chatImage.src == siteURL + "images/chat1.png") {
+  //split the returned text at the / after "images" and store what comes after it
+  const imageFileName = chatImage.src.split("/").pop();
+
+  if (imageFileName === "chat1.png") {
     console.log("working");
     chatImage.src = "images/chat2.png";
-  } else if (chatImage.src == siteURL + "images/chat2.png") {
+  } else if (imageFileName === "chat2.png") {
     chatImage.src = "images/chat3.png";
-  } else if (chatImage.src == siteURL + "images/chat3.png") {
+  } else if (imageFileName === "chat3.png") {
     window.location.href = "battle.html";
   }
 }
